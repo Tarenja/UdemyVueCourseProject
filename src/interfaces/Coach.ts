05266d1b2bd5f31be95591ca60fa87ad8196502c
Tarenja <tarenya@hotmail.com>
2021-05-26 16:18:46 +0200

    coachdetails page done

---
+++ src/interfaces/Coach.ts
@@ -0,0 +1,8 @@
+export default interface Coach {
+  id: String;
+  firstName: String;
+  lastName: String;
+  areas: String;
+  description: String;
+  hourlyRate: number;
+}
--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,13 +1,66 @@
 <template>
-  DETAILS
-  <router-view></router-view>
-  <router-link to="/coaches/c1/contact">Contact Coach</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLink">Contact</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import Coach from "../../interfaces/Coach";
 
-@Options({})
+@Options({
+  props: ["id"],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  created() {
+    this.selectedCoach = this.$store.getters["coaches/getCoaches"].find(
+      (coach: Coach) => coach.id === this.id
+    );
+  },
+  computed: {
+    fullName() {
+      return `${this.selectedCoach.firstName} ${this.selectedCoach.lastName}`;
+    },
+    contactLink() {
+      return `${this.$route.path}/${this.id}/contact`;
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+    description() {
+      return this.selectedCoach.description;
+    },
+  },
+})
 export default class CoachDetails extends Vue {}
 </script>
 
