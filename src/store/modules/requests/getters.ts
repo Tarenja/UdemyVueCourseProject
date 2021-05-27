b4d2c6e0028e1e93735f198ce2f36e2a63826c1d
Tarenja <tarenya@hotmail.com>
2021-05-27 16:12:23 +0200

    showing only current coach messages

--- src/store/modules/requests/getters.ts
+++ src/store/modules/requests/getters.ts
@@ -1,8 +1,11 @@
+import Request from '@/interfaces/Request';
+
 export default {
-  getRequests(state: any) {
-    return state.requests;
+  getRequests(state: any, _: any, _2: any, rootGetters: any) {
+    const coachId = rootGetters.getUserId;
+    return state.requests.filter((req: Request) => req.coachId === coachId);
   },
-  hasRequests(state: any) {
-    return state.requests && state.requests.length > 0;
+  hasRequests(_: any, getters: any) {
+    return getters.getRequests && getters.getRequests.length > 0;
   }
 };
